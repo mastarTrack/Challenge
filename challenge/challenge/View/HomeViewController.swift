033e77cf8676b85bad971861148707496fa5d112
Bin <ybin4548@gmail.com>
2026-03-16 10:36:14 +0900

    🩹chore: File 네이밍 변경

--- challenge/challenge/View/HomeViewController.swift
+++ challenge/challenge/View/HomeViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class MainViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
